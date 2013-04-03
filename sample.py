bdc9f9b5ee3f2b01783412dae3b2e030ead44a82
sowcod <sow.code.42+github@gmail.com>
2013-04-03 20:41:28 +0900

    Response code removed

--- sample.py
+++ sample.py
@@ -6,7 +6,6 @@ handler = pyserver.URLHandler()
 
 @handler.url('/$')
 def toppage(handler, match) :
-    handler.send_response(200)
     handler.send_headers()
     handler.wfile.write('<html><body><b>')
     handler.wfile.write('Welcome to pyserver')
@@ -14,14 +13,12 @@ def toppage(handler, match) :
 
 @handler.url('/json')
 def json(handler, match) :
-    handler.send_response(200)
     handler.send_headers({'Content-Type' : 'application/json'})
     handler.wfile.write('{"key1" : 1, "key2" : 2}')
 
 @handler.url('/streaming')
 def streaming(handler, match) :
     import time
-    handler.send_response(200)
     handler.send_headers({'Content-Type' : 'text/event-stream'})
     for i in range(10) :
         handler.wfile.write('response %d\r\n' % i)
