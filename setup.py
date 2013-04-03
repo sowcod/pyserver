02c687423dc8c47c6a931b792ad5603d71850444
sowcod <sow.code.42+github@gmail.com>
2013-04-03 20:01:01 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+import distutils.core
+
+distutils.core.setup(
+        name = 'pyserver', 
+        version = '0.1',
+        description = 'Simple HTTP Server',
+        author = 'sowcod',
+        author_email = 'sow.code.42+github@gmail.com',
+        url = 'https://github.com/sowcod/pyserver',
+        packages = ['pyserver']
+        )
